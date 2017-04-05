be6112f7331d863da03cb61fea399ab40e5f6af1
John Higgins Avila <johnhiggins.avila@gmail.com>
2017-04-05 09:25:38 +0800

    Update database.js

--- server/database/database.js
+++ server/database/database.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-//var myDb = 'postgresql://localhost:5432/1234/social_network_example';
+
 var sequelize = new Sequelize('obqa_development_db', 'username', 'password',{
   host:'127.0.0.1',
   dialect:'postgres'
